afa934e84693c328469cd31231b39f124d250848
Pedro Higuera <higueragarzonpedroarturo@gmail.com>
2021-12-08 10:33:01 -0500

    index actualizado

--- src/main.js
+++ src/main.js
@@ -5,4 +5,26 @@ import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core
 import { createApolloProvider } from '@vue/apollo-option'
 import { setContext } from 'apollo-link-context'
 
+const httpLink = createHttpLink({
+    uri: 'https://grupo3-api-gateway.herokuapp.com/',
+})
+
+const authLink = setContext((_, { headers }) => {
+    return {
+        headers: {
+            ...headers,
+            "Authorization": localStorage.getItem("token_access") || ""
+        }
+    }
+})
+
+const apolloClient = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache()
+})
+
+const apolloProvider = createApolloProvider({
+    defaultClient: apolloClient,
+})
+
 createApp(App).use(router).mount('#app')
