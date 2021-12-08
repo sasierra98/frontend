d21f43306eb0da0ba7d6c99a91bfbea13a5dc029
Pedro Higuera <higueragarzonpedroarturo@gmail.com>
2021-12-08 00:48:11 -0500

    Paquetes instalados

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client/core'
+import { createApolloProvider } from '@vue/apollo-option'
+import { setContext } from 'apollo-link-context'
 
 createApp(App).use(router).mount('#app')
