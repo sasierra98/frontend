8cd6ce7f049b765b820c1e230ae32b491351ade9
Pedro Higuera <higueragarzonpedroarturo@gmail.com>
2021-12-09 19:11:23 -0500

    Cambios

--- src/components/PetsSignUp.vue
+++ src/components/PetsSignUp.vue
@@ -1,14 +1,14 @@
 <template>
-  <div class="signUp_user">
-    <div class="container_signup_user">
+  <div class="signUp_pet">
+    <div class="container_signup_pet">
       <h1>Añadir Mascota</h1>
-      <form v-on:submit.prevent="proccessSignUp">
-        <input type="text" v-model="pet.id" placeholder="Identificación">
+      <form v-on:submit.prevent="proccessSignUpPet">
+        <input type="text" v-model="pet.id" placeholder="Identificacion">
         <input type="text" v-model="pet.imgBin64" placeholder="Imagen">
-        <input type="text" v-model="pet.descripcion" placeholder="Descripción">
+        <input type="text" v-model="pet.descripcion" placeholder="Descripcion">
         <input type="text" v-model="pet.fecha" readonly="readonly">
         <input type="text" v-model="pet.ciudad" placeholder="Ciudad">
-        <input type="number" v-model="pet.telefono" placeholder="Teléfono">
+        <input type="number" v-model="pet.telefono" placeholder="Telefono">
         <button type="submit">Añadir</button>
       </form>
     </div>
@@ -47,7 +47,7 @@ export default {
   },
 
   methods: {
-    proccessSignUp: async function(){
+    proccess: async function(){
       await this.$apollo
           .mutate({
             mutation: gql`
@@ -63,26 +63,25 @@ export default {
                     }
                 `,
             variables: {
-              input: this.pet /
+              input: this.pet 
             },
           })
           .then((result)=>{
-            let dataLogIn = {
-              username: this.user.username,
-              token_access: result.data.signUpUser.access,
-              token_refresh: result.data.signUpUser.refresh,
-            };
-            this.$emit("completedSignup", dataLogIn);
+            alert("Mascota añadida con éxito");
+            this.$router.push({name: 'pets'})
           }).catch((error)=>{
             alert("Error al realizar el registro");
           });
+    },
+    proccessSignUpPet: function(){
+      alert("Mascota añadida con éxito");
     }
   }
 }
 </script>
 
 <style>
-.signUp_user{
+.signUp_pet{
   margin: 0;
   padding: 0%;
   height: 100%;
@@ -92,7 +91,7 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.container_signup_user{
+.container_signup_pet{
   margin: 40px;
   padding: 20px;
   box-shadow: 0px 0px 10px #c7c7c7;
@@ -104,24 +103,24 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.container_signup_user h1{
+.container_signup_pet h1{
   margin: 10px;
 }
-.container_signup_user form{
+.container_signup_pet form{
   display: flex;
   flex-direction: column;
   justify-content: center;
 }
-.container_signup_user form input,select,button{
+.container_signup_pet form input,select,button{
   margin: 5px;
   width: 100%;
 }
-.container_signup_user form input,select{
+.container_signup_pet form input,select{
   border: 1px solid #c7c7c7;
   border-radius: 5px;
   padding: 10px;
 }
-.container_signup_user form button{
+.container_signup_pet form button{
   border: 1px solid #c7c7c7;
   border-radius: 5px;
   padding: 10px;
@@ -131,7 +130,7 @@ export default {
   transition: .3s;
   cursor: pointer;
 }
-.container_signup_user form button:hover{
+.container_signup_pet form button:hover{
   background-color: #FFFFFF;
   color: #3FBACC;
 }
